662fbacd448130e8a9c7bf85fb3c044f5e628ec4
Nicolas Rodriguez Lasso <nicolas.rlasso@gmail.com>
2021-05-04 08:08:16 -0500

    add Products screen and modify links props

--- frontent/src/App.js
+++ frontent/src/App.js
@@ -2,19 +2,21 @@ import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen'
 import Header from './components/Headers'
 import Footer from './components/Footer'
+import { BrowserRouter as Router ,Route } from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
-    <div >
+    <Router >
       <Header/>
       <main className="py-3">
         <Container>
-          <HomeScreen/>
-          <h1>welcome</h1>
+          <Route path='/' component={HomeScreen} exact/>
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer/>
-    </div>
+    </Router>
   );
 }
 
