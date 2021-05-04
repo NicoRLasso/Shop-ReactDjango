12d8fdf71ad23f10459753a0fea05db12398b67a
Nicolas Rodriguez Lasso <nicolas.rlasso@gmail.com>
2021-05-04 15:09:53 -0500

    moddify models and add serializers

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .products import products
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products,many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(request,pk):
