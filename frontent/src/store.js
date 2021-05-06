9587c348eb00cac55a6f497a665e5784c649d5b3
Nicolas Rodriguez Lasso <nicolas.rlasso@gmail.com>
2021-05-06 08:23:28 -0500

    add card const and reducer. through store state

--- frontent/src/store.js
+++ frontent/src/store.js
@@ -2,10 +2,12 @@ import { createStore,combineReducers,applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducers,productDetailsReducer} from './reducers/productReducers'
+import { cartReducer } from './reducers/cardReducer'
 
 const reducer = combineReducers({
     productList:productListReducers,
-    productDetails:productDetailsReducer
+    productDetails:productDetailsReducer,
+    card:cartReducer,
 });
 
 const initialState = {};
