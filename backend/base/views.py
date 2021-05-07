f30dcf20e36e9a441cf2213f33401464878761c3
Nicolas Rodriguez Lasso <nicolas.rlasso@gmail.com>
2021-05-07 15:35:59 -0500

    add signals to user model and urls

--- backend/base/views.py
+++ backend/base/views.py
@@ -10,6 +10,8 @@ from .serializers import ProductSerializer,UserSerializer,UserSerializerWithToke
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self,attrs):
@@ -30,11 +32,12 @@ def getProducts(request):
 
 @api_view(['GET'])
 def getProduct(request,pk):
-    product = User.objects.get(_id=pk)
-    serializer = ProductSerializer(products,many=False)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product,many=False)
     return Response(serializer.data)
 
 @api_view(['GET'])
+@permission_classes([IsAdminUser])
 def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users,many=True)
@@ -47,3 +50,19 @@ def getUserProfile(request):
     serializer = UserSerializer(user,many=False)
     return Response(serializer.data)
 
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user,many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail':'User with this email already exist'}
+        return Response(message,status=status.HTTP_400_BAD_REQUEST)
