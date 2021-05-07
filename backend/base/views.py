ee69b7a55e4e71ccc2885e2c8cedafcfb6d662f1
Nicolas Rodriguez Lasso <nicolas.rlasso@gmail.com>
2021-05-07 10:57:09 -0500

    Add Auth simpleJWT and methods for it

--- backend/base/views.py
+++ backend/base/views.py
@@ -3,7 +3,19 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer,UserSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self,attrs):
+        data =super().validate(attrs)
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['GET'])
 def getProducts(request):
@@ -15,4 +27,10 @@ def getProducts(request):
 def getProduct(request,pk):
     products = Product.objects.get(_id=pk)
     serializer = ProductSerializer(products,many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user,many=False)
     return Response(serializer.data)
